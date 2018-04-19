278d88cc9ad4bd97c9b2b1886ceeef21fd4973b2
Milo-Sobral <milo.sobral@mail.mcgill.ca>
2018-04-19 00:59:14 -0400

    Trying to get the create_data_set bash script to run

--- create_data_set.sh
+++ create_data_set.sh
@@ -1,13 +1,13 @@
 for image in `ls $1/*.jpg`
 do
-  mogrify -resize 20% image.jpg
-  java LoadImages.class image 1 dataset.txt
+  mogrify -resize 20% $image
+  java classes/LoadImages.class $image 1 dataset.txt
 done
 
 for image in `ls $2/*.jpg`
 do
-  mogrify -resize 20% image.jpg
-  java LoadImages.class image 1 dataset.txt
+  mogrify -resize 20% $image
+  java classes/LoadImages.class $image 2 dataset.txt
 done
 
 echo "done"
